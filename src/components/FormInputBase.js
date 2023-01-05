22240df9daf78f5123efd6356e5c7c1e816bc20e
10chi <tenaranogajec.1@gmail.com>
2023-01-05 10:47:06 +0100

    FormInputBase styling

--- src/components/FormInputBase.js
+++ src/components/FormInputBase.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useController } from "react-hook-form";
+import styled from 'styled-components'
 import { Input } from "./Input.js";
 
 export const FormInputBase = ({ control, defaultValue, fieldType, id, name, options, placeholder }) => {
@@ -9,8 +10,8 @@ export const FormInputBase = ({ control, defaultValue, fieldType, id, name, opti
 
 
     return (
-        <>
-            <h5>{name}</h5>
+        <StyledDivField>
+            <StyledH5Label>{name}</StyledH5Label>
             <Input
                 fieldType={fieldType}
                 id={id}
@@ -19,6 +20,17 @@ export const FormInputBase = ({ control, defaultValue, fieldType, id, name, opti
                 placeholder={placeholder}
                 value={value}
             />
-        </>
+        </StyledDivField>
     )
 }
+
+const StyledDivField = styled.div`
+    border: 1px solid green;
+    border-radius: 12px;
+    margin-top: 12px;
+    margin-bottom: 12px;
+    padding: 12px;
+`
+const StyledH5Label = styled.h5`
+    margin-top: 0;
+`
